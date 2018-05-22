91d00604627e60890ae2306c9e48547ef300b0c5
Xenia1309 <33964762+Xenia1309@users.noreply.github.com>
2018-05-22 10:31:36 +0300

    Основа проекта
    Консольное приложение и библиотека класса Lexer

---
+++ MathCadConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using SimpleLexer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathCadConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lexer = new MathLexer();
+
+            var tokens = lexer.Tokenize("2*2.3e-03");
+
+            foreach (var token in tokens)
+                Console.WriteLine(token);
+
+            Console.WriteLine("Press ENTER to quit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MathCadConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("MathCadConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MathCadConsole")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("4394b0d9-145b-435c-b567-f5f8b9162af6")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
